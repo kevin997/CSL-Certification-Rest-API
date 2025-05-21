c8896fcdd4719099c155fb57e35c02aca6909481
OVANGA LIBOIRE KEVIN <kevinliboire@gmail.com>
2025-05-21 13:10:27 +0100

    added fixes

---
+++ database/migrations/2025_05_21_114733_add_activity_id_to_certificate_contents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('certificate_contents', function (Blueprint $table) {
+            $table->unsignedBigInteger('activity_id')->after('id')->nullable();
+            $table->foreign('activity_id')->references('id')->on('activities')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('certificate_contents', function (Blueprint $table) {
+            $table->dropForeign(['activity_id']);
+            $table->dropColumn('activity_id');
+        });
+    }
+};
