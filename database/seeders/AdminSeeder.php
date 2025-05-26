042eb3804b5d1e616e2e7b1028e077038f293fbd
OVANGA LIBOIRE KEVIN <kevinliboire@gmail.com>
2025-05-26 08:43:28 +0100

    pulling from develop

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,12 +14,14 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@csl-certification.com',
-            'password' => Hash::make('Kwbi#1DG^FRE@1990!:$'), // Should be changed in production
-            'email_verified_at' => now(),
-            'role' => UserRole::SUPER_ADMIN,
-        ]);
+        if (!User::where('email', 'admin@csl-certification.com')->exists()) {
+            User::create([
+                'name' => 'Admin User',
+                'email' => 'admin@csl-certification.com',
+                'password' => Hash::make('Kwbi#1DG^FRE@1990!:$'), // Should be changed in production
+                'email_verified_at' => now(),
+                'role' => UserRole::SUPER_ADMIN,
+            ]);
+        }
     }
 }
