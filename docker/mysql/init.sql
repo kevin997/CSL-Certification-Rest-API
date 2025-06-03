9a61ee71239aa4a1eb12cfc16e0abd6e5a574a99
OVANGA LIBOIRE KEVIN <kevinliboire@gmail.com>
2025-06-03 08:47:43 +0100

    using root in mysql

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,13 +1,23 @@
 -- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS `cfpcwjwg_certification_api_db`;
 
--- Grant privileges to user from any host
-CREATE USER IF NOT EXISTS 'cfpcwjwg_certi_user'@'%' IDENTIFIED BY '#&H3k-ID0V';
+-- Drop and recreate user to ensure clean permissions
+DROP USER IF EXISTS 'cfpcwjwg_certi_user'@'%';
+DROP USER IF EXISTS 'cfpcwjwg_certi_user'@'localhost';
+DROP USER IF EXISTS 'cfpcwjwg_certi_user'@'172.%';
+
+-- Create user with proper permissions
+CREATE USER 'cfpcwjwg_certi_user'@'%' IDENTIFIED BY '#&H3k-ID0V';
 GRANT ALL PRIVILEGES ON `cfpcwjwg_certification_api_db`.* TO 'cfpcwjwg_certi_user'@'%';
 
--- Also grant privileges to root from any host
-UPDATE mysql.user SET host='%' WHERE user='root';
+-- Ensure root has proper permissions from any host
+DROP USER IF EXISTS 'root'@'172.%';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '#&H3k-ID0V';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
 -- Apply changes
 FLUSH PRIVILEGES;
+
+-- Verify users
+SELECT User, Host FROM mysql.user;
