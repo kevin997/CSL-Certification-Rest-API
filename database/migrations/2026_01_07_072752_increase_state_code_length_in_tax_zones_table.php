33239354d47be5ecff5f3b13e2df2ddc7a3d6c9a
OVANGA LIBOIRE KEVIN <kevinliboire@gmail.com>
2026-01-07 08:35:38 +0100

    fix : added more characters for tax states field and provied a valid container name to reverb

---
+++ database/migrations/2026_01_07_072752_increase_state_code_length_in_tax_zones_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tax_zones', function (Blueprint $table) {
+            $table->string('state_code', 20)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tax_zones', function (Blueprint $table) {
+            $table->string('state_code', 10)->nullable()->change();
+        });
+    }
+};
